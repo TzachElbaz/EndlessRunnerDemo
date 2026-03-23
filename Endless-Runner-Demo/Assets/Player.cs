583a08708b1e2fed60ed327050d3ad876e1d398d
Tsah Elbaz <tsah.elbaz@gmail.com>
2026-03-23 22:09:45 +0200

    ground object + ThresholdJump

--- Endless-Runner-Demo/Assets/Player.cs
+++ Endless-Runner-Demo/Assets/Player.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Jumping")]
     [SerializeField] private float gravity;
-    [SerializeField] private Vector2 velocity;
     [SerializeField] private float jumpVelocity = 20;
-    [SerializeField] private float groundHeight = 10;
     [SerializeField] private float maxHoldJumpTime = 0.4f;
+    [SerializeField] private float jumpGroundThreshold = 1;
 
+    private Vector2 velocity;
+    private float groundHeight = 10;
     private float holdJumpTimer = 0.0f;
     private bool isGrounded = true;
     private bool isHoldongJump = false;
@@ -23,7 +25,10 @@ public class Player : MonoBehaviour
     // For Inputs
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        Vector2 position = transform.position;
+        float groundDistance = Mathf.Abs(position.y - groundHeight);
+
+        if (Input.GetKeyDown(KeyCode.Space) && (isGrounded || groundDistance <= jumpGroundThreshold))
             JumpSettings();
         if (Input.GetKeyUp(KeyCode.Space))
             isHoldongJump = false;
@@ -47,11 +52,10 @@ public class Player : MonoBehaviour
             position.y += velocity.y * Time.fixedDeltaTime;
             if (!isHoldongJump)
                 velocity.y += gravity * Time.fixedDeltaTime;
-            if (position.y <= groundHeight)
+            if (position.y <= groundHeight) // onLanding
             {
                 position.y = groundHeight;
                 isGrounded = true;
-                holdJumpTimer = 0.0f;
             }
         }
 
@@ -63,5 +67,6 @@ public class Player : MonoBehaviour
         isGrounded = false;
         velocity.y = jumpVelocity;
         isHoldongJump = true;
+        holdJumpTimer = 0.0f;
     }
 }
