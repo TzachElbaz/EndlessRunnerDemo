b263f26c925b6c809ecb1a8ce12ba3d0c748fe14
Dolev888 <dolev007001@gmail.com>
2026-05-06 17:10:06 +0300

    bloop

--- Endless-Runner-Demo/Assets/Scripts/Collectables.cs
+++ Endless-Runner-Demo/Assets/Scripts/Collectables.cs
@@ -3,23 +3,15 @@ using UnityEngine;
 public class Collectables : MonoBehaviour
 {
     CollectablesManager collectables;
-    [SerializeField,Range(1,4)] private int _colNum;
-    
-    [SerializeField] SCREEN_COL _zone;
+    [SerializeField] private int _colectableId;   
     [SerializeField] SO_Collectable _so;
-    private int _ZoneNum;
-    private enum SCREEN_COL
-    {
-        FOREST,
-        DESERT
-    }
+    [SerializeField] Animator _animation;
+    
+   
 
     private void Awake()
     {
-        collectables = GameObject.FindAnyObjectByType<CollectablesManager>();
-        //_zone = _so._zone;
-
-        
+        collectables = GameObject.FindAnyObjectByType<CollectablesManager>();      
     }
     void Start()
     {
@@ -27,20 +19,9 @@ public class Collectables : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
-        {
-            switch (_zone)
-            {
-                case SCREEN_COL.FOREST:
-                    _ZoneNum = 0;
-                    break;
-                case SCREEN_COL.DESERT:
-                    _ZoneNum = 1;
-                    break;
-            }
-            collectables._colectableNum[_ZoneNum, _colNum-1] = true;
-            Destroy(gameObject);
-        }
+
+        collectables.SetCollectable(_colectableId);
+        Destroy(gameObject);
     }
 
     void Update()
--- Endless-Runner-Demo/Assets/Scripts/CollectablesManager.cs
+++ Endless-Runner-Demo/Assets/Scripts/CollectablesManager.cs
@@ -4,7 +4,8 @@ public class CollectablesManager : MonoBehaviour
 {
     public static CollectablesManager instance;
     public int _coinCount;
-    public bool[,] _colectableNum = new bool[2,4];
+    public bool[] _colectableList;
+    [SerializeField] private Animator[] _anmation;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,4 +17,24 @@ public class CollectablesManager : MonoBehaviour
     {
         
     }
+    public void SetCollectable(int id)
+    {
+        _colectableList[id] = true;
+
+        _anmation[id].SetBool("isVisible",true);
+        if (_colectableList[0] && _colectableList[1] && _colectableList[2] && _colectableList[3])
+        {
+            CollectableReset();
+        }
+        
+    }
+    public void CollectableReset()
+    {
+        for (int i = 0; i < _colectableList.Length; i++)
+        {
+            _colectableList[i] = false;
+            _anmation[i].SetBool("isVisible", false);
+        }
+    }
+
 }
